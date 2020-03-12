48abdaace8e48769c1c19750b3a36b24919044e3
Zack Matar <matar.zachary@gmail.com>
2020-03-11 22:43:44 -0700

    Initial commit

---
+++ LoanDepot Assestment/Module1.vb
@@ -0,0 +1,8 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine(Enumerable.Range(0, 1000).Where(Function(n) n Mod 3 = 0 Or n Mod 5 = 0).Sum())
+        Console.ReadLine()
+    End Sub
+
+End Module
